2c84d45a1ca1b36f8b7f8ab97a6893b388b1c34c
sashenkaaa26 <alexandra.mladinova@gmail.com>
2021-12-27 21:06:34 +0200

    document write

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let x = 5;
-alert(typeof x);
-let cat = {
-  name: 'Bars',
-  age: 2
-};
+// let x = 5;
+// alert(typeof x);
+// let cat = {
+//   name: 'Bars',
+//   age: 2
+// };
 
 
-alert(typeof cat);
-alert( typeof(cat));
+// alert(typeof cat);
+// alert( typeof(cat) );
+console.log( cat );
+document.write(username);
+document.write('<h2>' + username + '</h2>');
